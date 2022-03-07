7460d98a2d58c0b173e66be9d0645ee002925415
Daisei Yoshino <daisei.yoshino@sencorp.co.jp>
2022-03-07 17:15:19 +0900

    add promiseArrayIterator()

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+// 呼び出す毎に1つPromiseオブジェクトを返す関数
+function* promiseArrayIterator (promiseArray: Promise<any>[]): Generator<Promise<any>, Promise<any>, boolean> {
+  let tmp = [...promiseArray];
+  while (0 < tmp.length) {
+    yield tmp.shift();
+  }
+  return Promise.resolve()
+}
 
